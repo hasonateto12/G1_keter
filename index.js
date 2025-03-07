47ac11de8dbfbc9bb1ebe76efb7c4c4bc583fad1
hasonateto12 <hassunateto3@gmail.com>
2025-03-07 06:13:15 +0200

    הגדרת פורט לשרת

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+// npm i express body-parser ejs htmlspecialchars mysql2  slashes@2.0.0
+const port = 106719;
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+const path = require("path");
+app.use(bodyParser.urlencoded({extended: false}));
+
+let db_M = require('./database');
+global.db_pool = db_M.pool;
+
+global.htmlspecialchars = require('htmlspecialchars');
+// const { addSlashes, stripSlashes } = require('slashes');
+
