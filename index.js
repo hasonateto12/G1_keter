a88119c0bbe6b46bc2cd1efe9e4126cf1eaf4276
hasonateto12 <hassunateto3@gmail.com>
2025-03-09 05:45:42 +0200

    ADD A app.use('/SH/',Shifts_R);

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // npm i express body-parser ejs htmlspecialchars mysql2  slashes@2.0.0
-const port = 106719;
+const port = 1067;
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -14,3 +14,14 @@ global.db_pool = db_M.pool;
 global.htmlspecialchars = require('htmlspecialchars');
 // const { addSlashes, stripSlashes } = require('slashes');
 
+
+const Employees_R = require('./Routers/Employees_R');
+app.use('/E/',Employees_R);
+
+const Shifts_R = require('./Routers/Shifts_R');
+app.use('/SH/',Shifts_R);
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
+});
